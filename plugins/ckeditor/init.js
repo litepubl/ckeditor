6a5e6ebf09edbdefe0077719c64b1639ff20041f
litepubl <yarrowsoft@gmail.com>
2016-07-13 20:29:38 +0300

    ckeditor-releases-full-4.5.10

--- plugins/ckeditor/init.js
+++ plugins/ckeditor/init.js
@@ -1,7 +1,7 @@
 (function( $, window, document){
 window.CKEDITOR_BASEPATH = '/plugins/ckeditor/ckeditor/';
 
-  $(document).ready(function() {
+  $(function() {
 $.load_script(ltoptions.files + "/plugins/ckeditor/ckeditor/ckeditor.js", function() {
 $.load_script(ltoptions.files + "/plugins/ckeditor/ckeditor/adapters/jquery.js", function() {
   $( 'textarea' ).ckeditor();
